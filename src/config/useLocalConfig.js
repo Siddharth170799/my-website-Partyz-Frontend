bcc5a0a362b7361ed2947757ee2fa2ce64fcc36e
siddharth <boorgusiddharth@gmail.com>
2025-05-03 16:34:07 +0530

    apis-changed

--- src/config/useLocalConfig.js
+++ src/config/useLocalConfig.js
@@ -1,16 +1,16 @@
-export const GetFunctionHalls = "http://localhost:4000/api/getFunctionHalls"
-export const GetBookingDateDetails = "http://localhost:4000/api/getBookingDateDetails"
-export const SupervisorIdPostingBackend = "http://localhost:4000/api/postSupervisorUserId"
-export const getFunctionHallsBySupervisorUserId = "http://localhost:4000/api/getFunctionHallsBySupervisorUserId"
-export const PostUserDetails =  "http://localhost:4000/api/postUserDetails"
-export const PostBookingDatesDetails =  "http://localhost:4000/api/postUserDetails"
-export const PostUserPhoneNumber = "http://localhost:4000/api/userPhoneNumber"
-export const getAllUserBookings =  "http://localhost:4000/api/getAllUserBookings"
-export const postBookingDate =  "http://localhost:4000/api/postBookingDate"
+export const GetFunctionHalls = "https://partyzbackendapplication-66iz.onrender.com/api/getFunctionHalls"
+export const GetBookingDateDetails = "https://partyzbackendapplication-66iz.onrender.com/api/getBookingDateDetails"
+export const SupervisorIdPostingBackend = "https://partyzbackendapplication-66iz.onrender.com/api/postSupervisorUserId"
+export const getFunctionHallsBySupervisorUserId = "https://partyzbackendapplication-66iz.onrender.com/api/getFunctionHallsBySupervisorUserId"
+export const PostUserDetails =  "https://partyzbackendapplication-66iz.onrender.com/api/postUserDetails"
+export const PostBookingDatesDetails =  "https://partyzbackendapplication-66iz.onrender.com/api/postUserDetails"
+export const PostUserPhoneNumber = "https://partyzbackendapplication-66iz.onrender.com/api/userPhoneNumber"
+export const getAllUserBookings =  "https://partyzbackendapplication-66iz.onrender.com/api/getAllUserBookings"
+export const postBookingDate =  "https://partyzbackendapplication-66iz.onrender.com/api/postBookingDate"
 
 
 // Define the base URL
-export const BASE_URL = "http://localhost:4000/api/updateRequestById/";
+export const BASE_URL = "https://partyzbackendapplication-66iz.onrender.com/api/updateRequestById/";
 
 // Create a function to generate the full URL
 export const getUpdateBookingRequestUrl = (itemId) => `${BASE_URL}${itemId}`;
